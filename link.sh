b6d92d07c95b5826a60376a3120810489e6d47fb
amoseui <amoseui@gmail.com>
2015-10-04 12:34:20 +0900

    apply ys theme in zsh
    Signed-off-by: amoseui <amoseui@gmail.com>

--- link.sh
+++ link.sh
@@ -4,6 +4,8 @@ DOTFILES_PATH=$(cd "$(dirname "$0")" && pwd)
 
 GITCONFIG_PATH="$DOTFILES_PATH/git/gitconfig"
 VIMRC_PATH="$DOTFILES_PATH/vim/vimrc"
+ZSHRC_PATH="$DOTFILES_PATH/zsh/zshrc"
 
 ln -fs $GITCONFIG_PATH $HOME/.gitconfig
 ln -fs $VIMRC_PATH $HOME/.vimrc
+ln -fs $ZSHRC_PATH $HOME/.zshrc
