24aee737d6590fe8644d29c2324dbc43bd169d1f
amoseui <amoseui@gmail.com>
2015-12-26 16:17:19 +0900

    add sublime settings preferences
    Signed-off-by: amoseui <amoseui@gmail.com>

--- link.sh
+++ link.sh
@@ -3,9 +3,11 @@
 DOTFILES_PATH=$(cd "$(dirname "$0")" && pwd)
 
 GITCONFIG_PATH="$DOTFILES_PATH/git/gitconfig"
+SUBLIME_PATH="$DOTFILES_PATH/sublime/Preferences.sublime-settings"
 VIMRC_PATH="$DOTFILES_PATH/vim/vimrc"
 ZSHRC_PATH="$DOTFILES_PATH/zsh/zshrc"
 
 ln -fs $GITCONFIG_PATH $HOME/.gitconfig
+ln -fs $SUBLIME_PATH $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 ln -fs $VIMRC_PATH $HOME/.vimrc
 ln -fs $ZSHRC_PATH $HOME/.zshrc
