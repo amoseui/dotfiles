35c87a6e978cc2b7a56144c1b1a79357f3867726
amoseui <amoseui@gmail.com>
2016-11-16 00:04:12 +0900

    [git] add global gitignore
    Signed-off-by: amoseui <amoseui@gmail.com>

--- link.sh
+++ link.sh
@@ -3,11 +3,13 @@
 DOTFILES_PATH=$(cd "$(dirname "$0")" && pwd)
 
 GITCONFIG_PATH="$DOTFILES_PATH/git/gitconfig"
+GITIGNORE_PATH="$DOTFILES_PATH/git/gitignore"
 SUBLIME_PATH="$DOTFILES_PATH/sublime/Preferences.sublime-settings"
 VIMRC_PATH="$DOTFILES_PATH/vim/vimrc"
 ZSHRC_PATH="$DOTFILES_PATH/zsh/zshrc"
 
 ln -fs $GITCONFIG_PATH $HOME/.gitconfig
+ln -fs $GITIGNORE_PATH $HOME/.gitignore
 ln -fs $SUBLIME_PATH $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
 ln -fs $VIMRC_PATH $HOME/.vimrc
 ln -fs $ZSHRC_PATH $HOME/.zshrc
