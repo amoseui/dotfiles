70365296b5c7f77814d16a7efd34bb8b5dd103fc
Amos Lim <amoseui@gmail.com>
2019-01-27 18:57:08 +0900

    install nvim

--- link.sh
+++ link.sh
@@ -7,7 +7,17 @@ GITIGNORE_PATH="$DOTFILES_PATH/git/gitignore"
 VIMRC_PATH="$DOTFILES_PATH/vim/vimrc"
 ZSHRC_PATH="$DOTFILES_PATH/zsh/zshrc"
 
-ln -fs $GITCONFIG_PATH $HOME/.gitconfig
-ln -fs $GITIGNORE_PATH $HOME/.gitignore
-ln -fs $VIMRC_PATH $HOME/.vimrc
-ln -fs $ZSHRC_PATH $HOME/.zshrc
+ln -sf $GITCONFIG_PATH ~/.gitconfig
+ln -sf $GITIGNORE_PATH ~/.gitignore
+ln -sf $ZSHRC_PATH ~/.zshrc
+
+mv -v ~/.vimrc ~/.vimrc.old 2> /dev/null
+ln -sf $VIMRC_PATH ~/.vimrc
+
+# nvim
+mkdir -p ~/.config/nvim/autoload
+ln -sf $VIMRC_PATH ~/.config/nvim/init.vim
+ln -sf ~/.vim/autoload/plug.vim ~/.config/nvim/autoload/
+
+vim +PlugInstall +qall
+
