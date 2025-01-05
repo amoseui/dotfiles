319a2929cc5751efbb4897bf74105aa103f1fe15
Amos Lim <amoseui@gmail.com>
2025-01-05 23:25:53 +0900

    [tmux] add tmux.conf

--- link.sh
+++ link.sh
@@ -4,11 +4,13 @@ DOTFILES_PATH=$(cd "$(dirname "$0")" && pwd)
 
 GITCONFIG_PATH="$DOTFILES_PATH/git/gitconfig"
 GITIGNORE_PATH="$DOTFILES_PATH/git/gitignore"
+TMUX_PATH="$DOTFILES_PATH/tmux/tmux.conf"
 VIMRC_PATH="$DOTFILES_PATH/vim/vimrc"
 ZSHRC_PATH="$DOTFILES_PATH/zsh/zshrc"
 
 ln -sf $GITCONFIG_PATH ~/.gitconfig
 ln -sf $GITIGNORE_PATH ~/.gitignore
+ln -sf $TMUX_PATH ~/.tmux.conf
 ln -sf $ZSHRC_PATH ~/.zshrc
 
 mv -v ~/.vimrc ~/.vimrc.old 2> /dev/null
